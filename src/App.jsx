e0b6acae58a72ee9b3d785c8cec68e2b8b1ae1f3
mohnapne <kuvaldin2012@gmail.com>
2024-09-11 19:59:19 +0200

    react intro

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// импорт css из отдельного файла
+import './App.css';
+
+// функции App является react компонентом
+// в return такая функция возвращает верстку с данными
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello, react! ⚡️</h1>
+    </div>
+  );
+}
+
+export default App;
