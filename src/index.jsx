930d5c57cb837433900b4368bfb4777c6c543b63
mohnapne <kuvaldin2012@gmail.com>
2024-09-11 20:24:01 +0200

    react intro

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // мы оборачиваем дочерние теги в родитель по просьбе react
+  // если нам не нужен явный контейнер мы можем воспользоваться пустым тегом - react фрагментом
   <>
     <App />
   </>
