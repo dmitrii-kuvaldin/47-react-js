e1e2fe0c238f6dea429675d9cd831ebe4802a79d
mohnapne <kuvaldin2012@gmail.com>
2024-09-13 20:12:25 +0200

    lazy

--- src/components/myButton/MyButton.jsx
+++ src/components/myButton/MyButton.jsx
@@ -1,7 +1,9 @@
 import "./myButton.css";
-function MyButton(props) {
-  console.log(props)
-  return <button className='myButton'>{props.text}</button>;
+
+// мы забираем переданные данные в круглых скобках
+// обычно мы используем деструктуризацию для того чтобы достать значения пропсов по ключам и положить их в соответствующие переменные
+function MyButton({text, func}) {
+  return <button onClick={func} className="myButton">{text}</button>;
 }
 
 export default MyButton;
